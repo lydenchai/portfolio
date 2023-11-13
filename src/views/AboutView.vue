021b1933c5737cde20a0b3e71a67726034532dbb
Lyden CHAI <lyden.chai@gmail.com>
2023-11-13 11:58:36 +0700

    Fix about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,39 +1,41 @@
 <template>
   <div class="w-full text-[#444444]">
-    <div
-      class="h-[100vh] md:h-auto w-full md:mt-[55px] flex justify-between items-center sm:flex-col md:mb-[50px] px-[20.5%] lg:px-[10%] md:px-[5%]"
-    >
-      <div class="w-[40%] sm:hidden">
-        <img src="../assets/imgs/Photos.jpg" alt="Avatar" class="w-[80%]" />
-      </div>
-      <div class="w-[60%] sm:w-[100%]">
-        <p class="letter-spacing-[0.3px] tracking-wide">
-          Hi, I'm <span class="text-[#416bd8] font-bold">Lyden CHAI</span>, and
-          I'm studying at
-          <a href="https://www.aeu.edu.kh/" target="_blank">
-            <span class="text-[#416bd8] font-bold cursor-pointer">
-              Asia Euro University
-            </span>
-          </a>
-          with a degree in Computer Science. My interests are in Front End
-          Engineering, and I love to create beautiful and performant products
-          with delightful user experiences.
-        </p>
-        <p class="mt-[30px] tracking-wide">
-          I'm interested in the Front End Engineer role at
-          <a href="https://www.intechdevkh.com/" target="_blank">
-            <span class="text-[#416bd8] font-bold cursor-pointer">
-              IntechDev
-            </span>
-          </a>
-          because I like working in this ridesharing space and creating products
-          to improve the lives of users.
-        </p>
-        <p class="mt-[30px] tracking-wide">
-          When I am not developing projects, I love gardening, reading,
-          traveling, photography, and listening to music.
-        </p>
-        <p class="mt-[30px] text-[25px] gap-4">🌱 📚 🌎 📸 🎧</p>
+    <div class="w-full h-[100vh] md:h-auto flex justify-between items-center">
+      <div
+        class="w-full flex sm:flex-col md:mt-[55px] md:mb-[50px] px-[20.5%] lg:px-[10%] md:px-[5%]"
+      >
+        <div class="w-[40%] sm:hidden">
+          <img src="../assets/imgs/Photos.jpg" alt="Avatar" class="w-[80%]" />
+        </div>
+        <div class="w-[60%] sm:w-[100%]">
+          <p class="letter-spacing-[0.3px] tracking-wide">
+            Hi, I'm <span class="text-[#416bd8] font-bold">Lyden CHAI</span>,
+            and I'm studying at
+            <a href="https://www.aeu.edu.kh/" target="_blank">
+              <span class="text-[#416bd8] font-bold cursor-pointer">
+                Asia Euro University
+              </span>
+            </a>
+            with a degree in Computer Science. My interests are in Front End
+            Engineering, and I love to create beautiful and performant products
+            with delightful user experiences.
+          </p>
+          <p class="mt-[30px] tracking-wide">
+            I'm interested in the Front End Engineer role at
+            <a href="https://www.intechdevkh.com/" target="_blank">
+              <span class="text-[#416bd8] font-bold cursor-pointer">
+                IntechDev
+              </span>
+            </a>
+            because I like working in this ridesharing space and creating
+            products to improve the lives of users.
+          </p>
+          <p class="mt-[30px] tracking-wide">
+            When I am not developing projects, I love gardening, reading,
+            traveling, photography, and listening to music.
+          </p>
+          <p class="mt-[30px] text-[25px] gap-4">🌱 📚 🌎 📸 🎧</p>
+        </div>
       </div>
     </div>
     <div
